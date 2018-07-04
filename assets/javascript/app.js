9e8ebb69a7bcd92a4b6e3d030c6cff1e841c913b
RafaelCAntunes <30886398+RafaelCAntunes@users.noreply.github.com>
2018-07-03 22:16:42 -0300

    input form created
    adds a form to receive user input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,16 @@
-import axios from "axios";
-import md5 from "md5";
+import search from "./character-search";
 
-const ts = new Date().getTime();
-const publicKey = "2bb31ab5663084a468dae992cb5fe0a3";
-const privateKey = "74756bc374f2c58e52db7a9a4d15f631bd6699ba";
+const form = document.querySelectorAll('[data-form]');
 
-const hash = md5(`&{ts}${privateKey}${publicKey}`);
+form[0].addEventListener('submit', event => {
+event.preventDefault();
 
-const instance = axios.create({
+const formComponent = event.target;
 
-  baseURL:`https://gateway.marvel.com:443/v1/public/characters?name=deadpool&apikey=${publicKey}&hash=${hash}`,
-  timeout: 10000
+const formData = new FormData(formComponent);
+
+search(formData.get('heroName'));
+
+
+
 });
-
-instance.get()
-.then(response => console.log(response))
-.catch(error => console.log(error));
